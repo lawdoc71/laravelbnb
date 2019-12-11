5472883c9a2cfbe945d2aabc7f1d986514ad63d1
lawdoc71 <mdbutlerlaw@me.com>
2019-12-11 10:25:51 -0800

    lesson-14 bootstrap styling & Mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravelbnb</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
