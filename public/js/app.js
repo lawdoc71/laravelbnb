3a4ff3cfa31feaeea50aaaf12d02f3374c3c320e
lawdoc71 <mdbutlerlaw@me.com>
2019-12-11 10:18:19 -0800

    lesson-13 Router link component and vue routing

--- public/js/app.js
+++ public/js/app.js
@@ -37287,7 +37287,17 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [_c("router-view")], 1)
+  return _c(
+    "div",
+    [
+      _c("router-link", { attrs: { to: "/" } }, [_vm._v("Home")]),
+      _vm._v(" "),
+      _c("router-link", { attrs: { to: "/second" } }, [_vm._v("Second")]),
+      _vm._v(" "),
+      _c("router-view")
+    ],
+    1
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
