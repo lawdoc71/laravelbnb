5bf4bc3564359b0be42462249a5aaab95cb3470c
lawdoc71 <mdbutlerlaw@me.com>
2019-12-13 06:54:19 -0800

    lesson-21 lifecycle hooks

--- resources/js/bookables/BookableListItem.vue
+++ resources/js/bookables/BookableListItem.vue
@@ -1,16 +1,17 @@
 <template>
-    <div>
-        <h1>{{ itemTitle }}</h1>
-        <p>{{ itemContent }}</p>
-    </div>
+  <div>
+    <h1>{{ itemTitle }}</h1>
+    <p>{{ itemContent }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    props: { itemTitle: String, itemContent: String, price: Number },
-    mounted() {
-        console.log(this.itemTitle);
-        this.itemTitle = "New Title"
-    }
+  props: { itemTitle: String, itemContent: String, price: Number },
+  mounted() {
+    console.log(this.itemTitle);
+    // Uncomment to see why modifying prop is a bad idea!
+    // this.itemTitle = "New title";
+  }
 };
 </script>
--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -1,23 +1,37 @@
 <template>
-    <div>
-        <bookable-list-item
-            item-title="Cheap Villa"
-            item-content="A very cheap villa"
-            :price="1000"
-        ></bookable-list-item>
-        <bookable-list-item
-            item-title="Cheap Villa 2"
-            item-content="A very cheap villa 2"
-            :price="1500"
-        ></bookable-list-item>
-    </div>
+  <div>
+    <bookable-list-item item-title="Cheap Villa" item-content="A very cheap villa" :price="1000"></bookable-list-item>
+    <bookable-list-item
+      item-title="Cheap Villa 2"
+      item-content="A very cheap villa 2"
+      :price="1500"
+    ></bookable-list-item>
+  </div>
 </template>
 
 <script>
 import BookableListItem from "./BookableListItem";
 export default {
-    components: {
-        BookableListItem
-    }
-}
+  components: {
+    BookableListItem
+  },
+//   beforeCreate() {
+//     console.log("before create");
+//   },
+  created() {
+    console.log("created");
+  },
+//   beforeMount() {
+//     console.log("before mount");
+//   },
+  mounted() {
+    console.log("mounted");
+  }
+//   beforeDestroy() {
+//     console.log("before destroy");
+//   },
+//   destroyed() {
+//     console.log("destroyed");
+//   }
+};
 </script>
