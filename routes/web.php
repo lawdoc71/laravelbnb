1da82f15250256ed7f712a3fb3100a103c7c9517
lawdoc71 <mdbutlerlaw@me.com>
2019-12-13 09:42:29 -0800

    lesson-32 Routes and model all() method

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+// this handles all routes except those that start with API
 Route::get('/{any?}', function () {
     return view('welcome');
 })->where('any', '^(?!api\/)[\/\w\.-]*'); // not 100% sure this is correct
