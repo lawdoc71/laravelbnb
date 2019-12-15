0af2e41a07b2454c773f412cf538c931e0a3cc60
lawdoc71 <mdbutlerlaw@me.com>
2019-12-15 08:30:04 -0800

    lesson-45 API Resources

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,21 +1,19 @@
 <template>
-    <div class="row">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                    <div v-if="!loading">
-                        <h2>{{ bookable.title }}</h2>
-                        <hr />
-                        <article>{{ bookable.description }}</article>
-                    </div>
-                    <div v-else>Loading. . . </div>
-                </div>
-            </div>
+  <div class="row">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-body">
+          <div v-if="!loading">
+            <h2>{{ bookable.title }}</h2>
+            <hr />
+            <article>{{ bookable.description }}</article>
+          </div>
+          <div v-else>Loading...</div>
         </div>
-        <div class="col-me-4">
-            Availablity & Prices
-        </div>
+      </div>
     </div>
+    <div class="col-md-4">availability & prices</div>
+  </div>
 </template>
 
 <script>
@@ -27,10 +25,10 @@ export default {
     };
   },
   created() {
-      this.loading = true;
+    this.loading = true;
     axios.get(`/api/bookables/${this.$route.params.id}`).then(response => {
-        this.bookable = response.data;
-        this.loading = false;
+      this.bookable = response.data.data;
+      this.loading = false;
     });
   }
 };
--- resources/js/bookables/Bookables.vue
+++ resources/js/bookables/Bookables.vue
@@ -19,7 +19,6 @@
 
 <script>
 import BookableListItem from "./BookableListItem";
-
 export default {
   components: {
     BookableListItem
@@ -48,19 +47,8 @@ export default {
   },
   created() {
     this.loading = true;
-
-    const p = new Promise((resolve, reject) => {
-      console.log(resolve);
-      console.log(reject);
-      setTimeout(() => resolve("Hello"), 3000);
-    })
-      .then(result => "Hello again " + result)
-      .then(result => console.log(result))
-      .catch(result => console.log(`Error ${result}`));
-    console.log(p);
-
     const request = axios.get("/api/bookables").then(response => {
-      this.bookables = response.data;
+      this.bookables = response.data.data;
       this.loading = false;
     });
   }
